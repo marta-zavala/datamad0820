9394434b13b5c17947455e059e8352566edbcd67
marta-zavala-dediego <martazavala95@hotmail.com>
2020-08-20 17:42:28 +0200

    Adding lab-errhand_listcomp

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -7,103 +7,129 @@ my_listComprehension = [1/egg for egg in eggs]
 print(my_listComprehension)
 
 #Insert here the module/library import statements 
-
-
+import math
+import random
 
 
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
-
-
-
+print("\nEjercicio 1:")
+square=[i**2 for i in range (1,21)]
+print(square)
 
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
 # Remember to use list comprehensions and to print your results
-
-
-
+print("\nEjercicio 2:")
+power_of_two = [numero**2 for numero in  range(1,51)]
+print(power_of_two)
 
 #3. Calculate the square root of the first 100 numbers. Use sqrt as the name of the list.
 # You will probably need to install math library with pip and import it in this file.  
 # Remember to use list comprehensions and to print your results
-
+print("\nEjercicio 3:")
+sqrt = [math.sqrt(i) for i in range(1,101)]
+print(sqrt)
 
 
 
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
-
-
+print("\nEjercicio 4:")
+my_list = [i for i in range(-10,1)]
+print(my_list)
 
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
-
+print("\nEjercicio 5:")
+odds = [i for i in range (1,101) if i%2==1]
+print(odds)
 
 
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
 # Remember to use list comprehensions and to print your results
-
+print("\nEjercicio 6:")
+divisible_by_seven = [i for i in range (1,1001) if i%7==0]
+print(divisible_by_seven) 
 
 
 
 #7. Remove all of the vowels in a string. Hint: make a list of the non-vowels. Use non_vowels as the name of the list.
 # Remember to use list comprehensions and to print your results
 # You can use the following test string but feel free to modify at your convenience
-
+print("\nEjercicio 7:")
 teststring = 'Find all of the words in a string that are monosyllabic'
-
-
+vowels = ['a','e','i','o','u','A','E','I','O','U']
+non_vowels = ["" if i in vowels else i for i in teststring]
+print(non_vowels)
 
 
 #8. Find the capital letters (and not white space) in the sentence 'The Quick Brown Fox Jumped Over The Lazy Dog'. 
 # Use capital_letters as the name of the list.  
 # Remember to use list comprehensions and to print your results
-
+print("\nEjercicio 8:")
+sentence = 'The Quick Brown Fox Jumped Over The Lazy Dog'
+capital_letters = [i for i in sentence if i.lower() != i]
+print(capital_letters)
 
 
 
 #9. Find all the consonants in the sentence 'The quick brown fox jumped over the lazy dog'.
 # Use consonants as the name of the list.
 # Remember to use list comprehensions and to print your results.
-
-
-
+print("\nEjercicio 9:")
+sentence1 = 'The quick brown fox jumped over the lazy dog'
+consonants = ["" if i in vowels else i for i in sentence1]
+print(consonants)
 
 
 #10. Find the folders you have in your madrid-oct-2018 local repo. Use files as name of the list.  
 # You will probably need to import os library and some of its modules. You will need to make some online research.
 # Remember to use list comprehensions and to print your results.
+print("\nEjercicio 10:")
+
 
 
 
 #11. Create 4 lists of 10 random numbers between 0 and 100 each. Use random_lists as the name of the list. 
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
+print("\nEjercicio 11:")
+def aleatorio(a,b):
+    return [random.randint(a,b) for i in range (10)]
 
-
+random_lists =[aleatorio(0,101),aleatorio(0,101),aleatorio(0,101),aleatorio(0,101)]
+print(random_lists)
 
 
 #12. Flatten the following list of lists. Use flatten_list as the name of the output.
 # Remember to use list comprehensions and to print your results
+print("\nEjercicio 12:")
+
 
 list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
+flatten_list = [e for sub in list_of_lists for e in sub]
+print(flatten_list)
 
 
 
 #13. Convert the numbers of the following nested list to floats. Use floats as the name of the list. 
 # Remember to use list comprehensions and to print your results.
+print("\nEjercicio 13:")
 
-list_of_lists = [['40', '20', '10', '30'], ['20', '20', '20', '20', '20', '30', '20'], \
-['30', '20', '30', '50', '10', '30', '20', '20', '20'], ['100', '100'], ['100', '100', '100', '100', '100'], \
+list_of_lists = [['40', '20', '10', '30'], ['20', '20', '20', '20', '20', '30', '20'],
+['30', '20', '30', '50', '10', '30', '20', '20', '20'], ['100', '100'], ['100', '100', '100', '100', '100'],
 ['100', '100', '100', '100']]
+floats = [float(e) for sub in list_of_lists for e in sub]
+print(floats)
 
 
 
 
+'''
 #14. Handle the exception thrown by the code below by using try and except blocks. 
-
+print("\nEjercicio 14:")
 
 for i in ['a','b','c']:
     print i**2
@@ -112,6 +138,7 @@ for i in ['a','b','c']:
 #15. Handle the exception thrown by the code below by using try and except blocks. 
 #Then use a finally block to print 'All Done.'
 # Check in provided resources the type of error you may use. 
+print("\nEjercicio 15:")
 
 x = 5
 y = 0
@@ -187,5 +214,4 @@ def linux_interaction():
 
 Total_Marks = int(input("Enter Total Marks Scored: ")) 
 Num_of_Sections = int(input("Enter Num of Sections: "))
-
-
+'''
