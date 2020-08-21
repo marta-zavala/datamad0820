517918893b770aec83ece01b2cf3e12244528832
marta-zavala-dediego <martazavala95@hotmail.com>
2020-08-21 18:42:26 +0200

    Adding changes tests war

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -26,7 +26,7 @@ class Viking (Soldier):
         if self.health >0:
             return f"{self.name} has received {damage} points of damage"
         else:
-            return (f"{self.name} has died in act of combat")
+            return f"{self.name} has died in act of combat"
     def battleCry(self):
         return "Odin Owns You All!"
 
@@ -47,6 +47,37 @@ class Saxon (Soldier):
 
 # War
 
+import random
 
 class War:
-    pass
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+    def addViking(self, Viking):
+        self.vikingArmy.append(Viking)
+    def addSaxon(self, Saxon):
+        self.saxonArmy.append(Saxon)
+
+    def vikingAttack(self):
+        vikingSoldier = random.choice(self.vikingArmy)
+        saxonSoldier = random.choice(self.saxonArmy)
+        saxonSoldier.receiveDamage(vikingSoldier.attack())
+        if saxonSoldier.health <=0:
+            self.saxonArmy.remove(saxonSoldier)
+        return f"result of calling {vikingSoldier.attack()} of a Saxon with the strength of a Viking"
+
+    def saxonAttack(self):
+        vikingSoldier = random.choice(self.vikingArmy)
+        saxonSoldier = random.choice(self.saxonArmy)
+        vikingSoldier.receiveDamage(saxonSoldier.attack())
+        if vikingSoldier.health <=0:
+            self.vikingArmy.remove(vikingSoldier)
+        return f"result of calling {saxonSoldier.attack()} of a Viking with the strength of a Saxon"
+
+    def showStatus(self):
+        if len(self.saxonArmy)==0:
+            return "Vikings have won the war of the century!"
+        elif len(self.vikingArmy)==0:
+            return "Saxons have fought for their lives and survive another day..."
+        elif len(self.saxonArmy)==1 and len(self.vikingArmy)==1:
+            return "Vikings and Saxons are still in the thick of battle."
