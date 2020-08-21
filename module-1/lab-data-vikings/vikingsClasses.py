5defdb92838d7c35d62232acc403e774c14630d6
marta-zavala-dediego <martazavala95@hotmail.com>
2020-08-21 16:43:19 +0200

    Adding tests vikings

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -16,7 +16,20 @@ class Soldier:
 # Viking
 
 
-class Viking:
+class Viking (Soldier):
+    def __init__(self, name, health, strength):
+        self.health = health
+        self.strength = strength
+        self.name = name
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health >0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return (f"{self.name} has died in act of combat")
+    def battleCry(self):
+        return "Odin Owns You All!"
+
     
 
 # Saxon
