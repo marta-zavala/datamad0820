808818dc5c7d3804ac56d6e0ef0c39f60836da16
marta-zavala-dediego <martazavala95@hotmail.com>
2020-08-26 18:52:36 +0200

    Adding Challenge 2

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,4 +1,4 @@
-#CHALLENGE1
+#CHALLENGE 1
 
 SELECT au.au_id AS 'AUTHOR ID', au.au_lname AS 'LAST NAME', au.au_fname AS 'FIRST NAME', t.title AS 'TITLE', p.pub_name AS 'PUBLISHER'
 	FROM authors AS au
@@ -9,4 +9,19 @@ SELECT au.au_id AS 'AUTHOR ID', au.au_lname AS 'LAST NAME', au.au_fname AS 'FIRS
 	INNER JOIN publishers AS p
 		ON t.pub_id = p.pub_id
     ORDER BY 'AUTHOR ID' ASC
+
+
+#CHALLENGE 2
+
+SELECT au.au_id AS 'AUTHOR ID', au.au_lname AS 'LAST NAME', au.au_fname AS 'FIRST NAME', COUNT(t.title) AS 'TITLE COUNT', p.pub_name AS 'PUBLISHER'
+	FROM authors AS au
+	INNER JOIN titleauthor AS tta
+		ON au.au_id = tta.au_id
+	INNER JOIN titles AS t
+		ON tta.title_id = t.title_id
+	INNER JOIN publishers AS p
+		ON t.pub_id = p.pub_id
+    GROUP BY au.au_id, au.au_lname, au.au_fname, p.pub_name
+
+#CHALLENGE 3
 
