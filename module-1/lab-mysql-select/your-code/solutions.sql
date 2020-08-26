f82027edaf4c91063f86f30b9ac3c7ba42a81cdc
marta-zavala-dediego <martazavala95@hotmail.com>
2020-08-26 19:42:25 +0200

    Adding Challenge 3

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,4 +1,4 @@
-#CHALLENGE 1
+/*  CHALLENGE 1  */
 
 SELECT au.au_id AS 'AUTHOR ID', au.au_lname AS 'LAST NAME', au.au_fname AS 'FIRST NAME', t.title AS 'TITLE', p.pub_name AS 'PUBLISHER'
 	FROM authors AS au
@@ -11,7 +11,7 @@ SELECT au.au_id AS 'AUTHOR ID', au.au_lname AS 'LAST NAME', au.au_fname AS 'FIRS
     ORDER BY 'AUTHOR ID' ASC
 
 
-#CHALLENGE 2
+/*  CHALLENGE 2  */
 
 SELECT au.au_id AS 'AUTHOR ID', au.au_lname AS 'LAST NAME', au.au_fname AS 'FIRST NAME', COUNT(t.title) AS 'TITLE COUNT', p.pub_name AS 'PUBLISHER'
 	FROM authors AS au
@@ -23,5 +23,16 @@ SELECT au.au_id AS 'AUTHOR ID', au.au_lname AS 'LAST NAME', au.au_fname AS 'FIRS
 		ON t.pub_id = p.pub_id
     GROUP BY au.au_id, au.au_lname, au.au_fname, p.pub_name
 
-#CHALLENGE 3
+/*  CHALLENGE 3  */
 
+SELECT au.au_id as 'AUTHOR ID', au.au_lname AS 'LAST NAME', au.au_fname AS 'FIRST NAME', SUM(s.qty) AS 'TOTAL'
+	FROM authors as au
+	INNER JOIN titleauthor AS tta
+		ON au.au_id = tta.au_id
+	INNER JOIN sales AS s
+		ON tta.title_id = s.title_id
+    GROUP BY au.au_id
+    ORDER BY SUM(s.qty) DESC LIMIT 3;
+
+
+/*  CHALLENGE 4  */
